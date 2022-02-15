8686a2b721bdf67b2e6b9ae97e108b3e06752fe3
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-15 14:12:47 +0100

    refact: change javascript to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-const dirname = require('path').dirname
+import { dirname } from 'path'
+import { IconsaucePlugin } from '@iconsauce/core/src/interface/plugin'
 
-module.exports = {
+export const googleMaterialIconsPlugin: IconsaucePlugin = {
   prefix: 'gm',
   regex: {
     code: /(gm)(\/{1}[a-z-]+){1,2}/gm,
     lib: /(([a-zA-Z_\-/]+\/)(filled|outline|round|sharp)[a-zA-Z_\-/]+\/ic_([0-9a-zA-Z_]+)_24px\.svg)/,
   },
-  selector: (path) => `gm/${path[3]}/${path[4].replace(/[_]+/g, '-')}`,
+  selector: (path: RegExpMatchArray) => `gm/${path[3]}/${path[4].replace(/[_]+/g, '-')}`,
   path: `${dirname(require.resolve('material-design-icons-updated/package.json'))}/**/+(filled|outline|round|sharp)/**/*_24px.svg`,
 }
