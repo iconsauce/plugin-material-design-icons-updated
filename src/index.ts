7957a553106199e6dbab43937bec877cae4ae016
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-17 12:45:15 +0100

    fix: make build simplier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { dirname } from 'path'
 import { IconsaucePlugin } from '@iconsauce/plugin'
 
-export const googleMaterialIconsPlugin: IconsaucePlugin = {
-  prefix: 'gm',
+export const materialDesignIconsUpdatedPlugin: IconsaucePlugin = {
+  prefix: 'miu',
   regex: {
-    code: /(gm)(\/{1}[0-9a-z-]+){1,2}/gm,
+    code: /(miu)(\/{1}[0-9a-z-]+){1,2}/gm,
     lib: /(([a-zA-Z_\-/]+\/)(filled|outline|round|sharp)[a-zA-Z_\-/]+\/ic_([0-9a-zA-Z_]+)_24px\.svg)/,
   },
-  selector: (path: RegExpMatchArray) => `gm/${path[3]}/${path[4].replace(/[_]+/g, '-')}`,
+  selector: (path: RegExpMatchArray) => `miu/${path[3]}/${path[4].replace(/[_]+/g, '-')}`,
   path: `${dirname(require.resolve('material-design-icons-updated/package.json'))}/**/+(filled|outline|round|sharp)/**/*_24px.svg`,
 }
