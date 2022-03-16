988f9237f9508df5535c244c48a9bdac1a3aa872
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-16 16:24:46 +0100

    fix: ease how modules are resolved

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const materialDesignIconsUpdatedPlugin: IconsaucePlugin = {
     lib: /(([a-zA-Z_\-/]+\/)(filled|outline|round|sharp)[a-zA-Z_\-/]+\/ic_([0-9a-zA-Z_]+)_24px\.svg)/,
   },
   selector: (path: RegExpMatchArray) => `miu/${path[3]}/${path[4].replace(/[_]+/g, '-')}`,
-  path: `${resolve('node_modules', 'material-design-icons-updated')}/**/+(filled|outline|round|sharp)/**/*_24px.svg`,
+  path: `${resolve(__dirname, '../../../material-design-icons-updated')}/**/+(filled|outline|round|sharp)/**/*_24px.svg`,
 }
 
 export default materialDesignIconsUpdatedPlugin
